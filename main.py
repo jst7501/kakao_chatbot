457c9e6f30ad7aa77f2943d8476314ad757de47a
jst7501 <cfku0172@gmail.com>
2022-07-12 18:58:26 +0900

    월급날까지 사이드 프로젝트 시작

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask  # 서버 구현을 위한 Flask 객체 import
+from flask import Flask, request  # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
 import json
 
@@ -8,8 +8,8 @@ api = Api(app)  # Flask 객체에 Api 객체 등록
 
 @api.route('/hello')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
 class HelloWorld(Resource):
-    def post(self, name):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
-        print(name)
+    def post(self):
+        print(request.json)
         a = {"version": "2.0",
              "template": {
                  "outputs": [
