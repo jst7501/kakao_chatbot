bb77e5163ff0b7f23654ea4806614c1d0341045a
jst7501 <cfku0172@gmail.com>
2022-07-12 16:36:56 +0900

    월급날까지 사이드 프로젝트 시작

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from flask import Flask  # 서버 구현을 위한 Flask 객체 import
-from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
+from fastapi import FastAPI  # FastAPI 모듈 가져오기
 
-app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
-api = Api(app)  # Flask 객체에 Api 객체 등록
+app = FastAPI()  # 객체 생성
 
 
-@api.route('/hello')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
-class HelloWorld(Resource):
-    def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
-        return {"hello": "world!"}
+@app.get("/")  # Route Path
+def test_index():
+
+    # Json 타입으로 값 반환
+    return {
+        "Python": "Framework",
+    }
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+@app.get("/something")
+def something():
+    return {
+        "Something": "Page",
+    }
